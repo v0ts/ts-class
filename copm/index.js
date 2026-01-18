c15e062719326ef69c0ad55909b2cfc412f196fe
VoTs <126974582+v0ts@users.noreply.github.com>
2026-01-18 15:53:10 +0200

    done

---
+++ copm/index.js
@@ -0,0 +1,67 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Key = (function () {
+    function Key() {
+        this.signature = Math.random();
+    }
+    Key.prototype.getSignature = function () {
+        return this.signature;
+    };
+    return Key;
+}());
+var Person = (function () {
+    function Person(key) {
+        this.key = key;
+    }
+    Person.prototype.getKey = function () {
+        return this.key;
+    };
+    return Person;
+}());
+"";
+var House = (function () {
+    function House(key) {
+        this.door = false;
+        this.tenants = [];
+        this.key = key;
+    }
+    House.prototype.comeIn = function (person) {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    };
+    return House;
+}());
+var MyHouse = (function (_super) {
+    __extends(MyHouse, _super);
+    function MyHouse() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    MyHouse.prototype.openDoor = function (key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    };
+    return MyHouse;
+}(House));
+var key = new Key();
+var house = new MyHouse(key);
+var person = new Person(key);
+house.openDoor(person.getKey());
+house.comeIn(person);
+console.log("Мешканці:", house["tenants"].length);
+console.log("Двері відкриті:", house["door"]);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+class Key {
+	private signature: number;
+
+	constructor() {
+		this.signature = Math.random();
+	}
+
+	getSignature(): number {
+		return this.signature;
+	}
+}
+
+class Person {
+	private key: Key;
+
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	getKey(): Key {
+		return this.key;
+	}
+}``
+abstract class House {
+	protected door: boolean = false;
+	protected key: Key;
+	protected tenants: Person[] = [];
+
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person);
+		}
+	}
+
+	abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true;
+		}
+	}
+}
+
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+house.comeIn(person);
+
+console.log("Мешканці:", house["tenants"].length);
+console.log("Двері відкриті:", house["door"]);
